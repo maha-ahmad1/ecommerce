2d1ef5425dd2d7ed22f8994dc9aafbd2c9c35be7
maha-ahmad1 <maha.ahmad.k@hotmail.com>
2023-08-14 19:05:26 +0300

    edit

--- src/layouts/MainLayout/components/NavLinks/index.tsx
+++ src/layouts/MainLayout/components/NavLinks/index.tsx
@@ -1,30 +1,30 @@
-import { Link } from "components";
-import { URL_PATHS } from "data";
+// import { Link } from "components";
+// import { URL_PATHS } from "data";
 
-const NavLinks = () => {
-  return (
-    // <div className="hidden md:block">
-<div>
-    <Link
-        href={URL_PATHS.HOME}
-        className="px-6 transition-colors hover:text-blue"
-      >
-        Home
-      </Link>
-      <Link
-        href={URL_PATHS.INVOICES.INDEX}
-        className="px-6 transition-colors hover:text-blue"
-      >
-        Invoices
-      </Link>
-      <Link
-        href={URL_PATHS.INVOICES.CREATE}
-        className="px-6 py-1 text-blue border rounded-full border-blue transition-colors hover:bg-blue hover:text-white"
-      >
-        Create
-      </Link>
-    </div>
-  );
-};
+// const NavLinks = () => {
+//   return (
+//     // <div className="hidden md:block">
+// <div>
+//     <Link
+//         href={URL_PATHS.HOME}
+//         className="px-6 transition-colors hover:text-blue"
+//       >
+//         Home
+//       </Link>
+//       <Link
+//         href={URL_PATHS.INVOICES.INDEX}
+//         className="px-6 transition-colors hover:text-blue"
+//       >
+//         Invoices
+//       </Link>
+//       <Link
+//         href={URL_PATHS.INVOICES.CREATE}
+//         className="px-6 py-1 text-blue border rounded-full border-blue transition-colors hover:bg-blue hover:text-white"
+//       >
+//         Create
+//       </Link>
+//     </div>
+//   );
+// };
 
-export default NavLinks;
+// export default NavLinks;
